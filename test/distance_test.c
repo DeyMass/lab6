4443cd417b248e4f107da67d4fc2578ad8560ff8
DeyMass <silkin.mixail@yandex.ru>
2016-03-30 09:18:08 +0600

    15

---
+++ test/distance_test.c
@@ -0,0 +1,45 @@
+#include <ctest.h>
+#include <discriminant.h>
+#include <solve.h>
+
+CTEST(distance_suite, NO_ROOTS)
+{
+	const double a = 4;
+	const double b = 0;
+	const double c = 3;
+	const double x1,x2;
+
+	const double distance = solve(a, b, c, &x1, &x2);
+
+	const double expected_d = 0;
+
+	ASSERT_DBL_NEAR(expected_d,distance);
+}
+
+CTEST(distance_suite, ONE_ROOTS)
+{
+	const double a = 1;
+	const double b = -2;
+	const double c = 1;
+	const double x1,x2;
+
+	const double distance = solve(a, b, c, &x1, &x2);
+
+	const double expected_d = 1;
+
+	ASSERT_DBL_NEAR(expected_d,distance);
+}
+
+CTEST(distance_suite, TWO_ROOTS)
+{
+	const double a = 2;
+	const double b = -3;
+	const double c = 1;
+	const double x1,x2;
+
+	const double distance = solve(a, b, c, &x1, &x2);
+
+	const double expected_d = 2;
+
+	ASSERT_DBL_NEAR(expected_d,distance);
+}
