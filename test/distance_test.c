de2d7ce78197bce90d1bb8654d3b4f471f71d7ef
Mikhail Silkin <mikhail.silkin@eltex.loc>
2018-10-05 11:09:13 +0700

    hel3

--- test/distance_test.c
+++ test/distance_test.c
@@ -2,22 +2,9 @@
 #include <discriminant.h>
 #include <solve.h>
 
-CTEST(distance_suite, NO_ROOTS)
-{
-	const double a = 4;
-	const double b = 0;
-	const double c = 3;
-	const double x1,x2;
-
-	const double distance = solve(a, b, c, &x1, &x2);
-
-	const double expected_d = 0;
-
-	ASSERT_DBL_NEAR(expected_d,distance);
-}
-
 CTEST(distance_suite, ONE_ROOTS)
 {
+	int ma;
 	const double a = 1;
 	const double b = -2;
 	const double c = 1;
